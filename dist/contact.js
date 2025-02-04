3e7e97bb38c9ff3027b70f9e24b246e4dd7c7c7c
idkwtp-1 <157178374+idkwtp-1@users.noreply.github.com>
2025-02-04 14:07:19 -0500

    docker

---
+++ dist/contact.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementById("contactForm");e&&e.addEventListener("submit",(e=>{e.preventDefault(),alert("Success")}))}))})();
